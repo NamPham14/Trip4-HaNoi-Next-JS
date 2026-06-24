1c532de7298060684f4563a6831d5f7c68579693
NamPham14 <nampham2662@gmail.com>
2026-06-24 11:47:55 +0700

    image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,10 +51,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/icon.jpg', type: 'image/jpeg' },
+      { url: '/app-icon-192.jpg', sizes: '192x192', type: 'image/jpeg' },
+      { url: '/app-icon-512.jpg', sizes: '512x512', type: 'image/jpeg' },
     ],
     apple: [
-      { url: '/icon.jpg', sizes: '180x180', type: 'image/jpeg' },
+      { url: '/app-icon-192.jpg', sizes: '180x180', type: 'image/jpeg' },
     ],
   },
   manifest: '/manifest.json',
